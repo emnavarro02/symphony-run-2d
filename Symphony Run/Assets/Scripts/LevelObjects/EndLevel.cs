e665f7187179dd62eeab4408111dd29387c8a301
emnavarro02 <emnavarro02@gmail.com>
2019-05-25 17:12:46 +0100

    new script EndLevel

---
+++ Symphony Run/Assets/Scripts/LevelObjects/EndLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision");
+
+        if (collision.CompareTag("Player"))
+        {
+            // Call endlevel
+            gameManager.EndLevel();
+        }
+    }
+}
