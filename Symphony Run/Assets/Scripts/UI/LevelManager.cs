33a13708a35e3304df913af88f685bb2d6c8fadf
emnavarro02 <emnavarro02@gmail.com>
2019-05-25 14:54:26 +0100

    added LevelManager

---
+++ Symphony Run/Assets/Scripts/UI/LevelManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LevelManager : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Level
+    {
+        public string levelText;
+        public int isUnlocked; //if 0 = locked; if 1 = unlocked
+        public bool isInteractable;
+
+       // public Button.ButtonClickedEvent OnClickEvent;
+    }
+
+    public GameObject levelButton;
+    public Transform spacer;
+    public List<Level> levelList;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // DeleteAll();
+        FillList();
+    }
+
+    void FillList()
+    {
+        foreach(var level in levelList)
+        {
+            GameObject newButton = Instantiate(levelButton) as GameObject;
+            LevelButton button = newButton.GetComponent<LevelButton>();
+            button.LevelText.text = level.levelText;
+
+            // The Level name must be: Level, Level2
+            if(PlayerPrefs.GetInt("Level" + button.LevelText.text) == 1)
+            {
+                //set Level to be unlocked
+                level.isUnlocked = 1;
+                level.isInteractable = true;
+            }
+
+            //update the button value
+            button.unlocked = level.isUnlocked;
+            button.GetComponent<Button>().interactable = level.isInteractable;
+
+            button.GetComponent<Button>().onClick.AddListener(() => loadLevels("Level" + button.LevelText.text));
+
+            Debug.Log("LevelText: " + button.LevelText.text);
+            Debug.Log("Value: " + PlayerPrefs.GetInt("Level" + button.LevelText.text + "_score"));
+            if (PlayerPrefs.GetInt("Level" + button.LevelText.text + "_score") >= 1)
+            {
+                button.Star1.SetActive(true);
+            }
+            if (PlayerPrefs.GetInt("Level" + button.LevelText.text + "_score") >= 2)
+            {
+                button.Star2.SetActive(true);
+            }
+            if (PlayerPrefs.GetInt("Level" + button.LevelText.text + "_score") == 3)
+            {
+                button.Star3.SetActive(true);
+            }
+
+            newButton.transform.SetParent(spacer);
+        }
+
+        SaveAll();
+    }
+
+    void SaveAll()
+    {
+        //if (PlayerPrefs.HasKey("Level1"))
+        //{
+        //    return;
+        //}
+        //else
+        {
+            GameObject[] allButtons = GameObject.FindGameObjectsWithTag("LevelButton");
+
+            foreach (GameObject btn in allButtons)
+            {
+                LevelButton button = btn.GetComponent<LevelButton>();
+
+                //Store Unlocked state
+                PlayerPrefs.SetInt("Level" + button.LevelText.text, button.unlocked);
+            }
+        }
+
+
+    }
+
+    void DeleteAll()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    void loadLevels(string value)
+    {
+        //Use scence manager
+        Application.LoadLevel(value);
+    }
+
+}
---
+++ Symphony Run/Assets/Scripts/UI/timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour
+{
+
+    int score = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("Level2", 1); // Key: LevelName, Value: 1=unlocked / 0=locked
+        PlayerPrefs.SetInt("Level1_score", score);
+        StartCoroutine(Time());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Time()
+    {
+        yield return new WaitForSeconds(10f);
+        //SceneManager.LoadScene(2);
+
+        //TODO: use SceneManager
+
+        //Back to the main screen
+        Application.LoadLevel(0);
+    }
+}
