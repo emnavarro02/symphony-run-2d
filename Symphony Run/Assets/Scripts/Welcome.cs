441f4753d342823d0d6f5374a42b2207c54597aa
emnavarro02 <emnavarro02@gmail.com>
2019-06-13 12:11:42 +0100

    Update Welcome.cs

--- Symphony Run/Assets/Scripts/Welcome.cs
+++ Symphony Run/Assets/Scripts/Welcome.cs
@@ -4,15 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class Welcome : MonoBehaviour
 {
-    [SerializeField]
-    private readonly int delay = 0;
-
-    private void Update()
+       private void Update()
     {
         //if (Input.GetMouseButtonDown(0))
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        //if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)) 
+        //if (Input.GetKeyDown(KeyCode.Space))
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)) 
         {
             print("Clicked");
             LoadMainScreen();
